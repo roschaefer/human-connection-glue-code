72b4cc435834ba7a56e716fd5b8222744133ca6a
Robert Schaefer <git@roschaefer.de>
2018-04-12 01:13:45 +0200

    Refactoring: Ecma script classes for user
    This will store the access token along with the user, for future
    requests.

--- index.mjs
+++ index.mjs
@@ -1,60 +1,69 @@
 import fetch from 'node-fetch';
 
-async function getFirstProcedure(){
-  const graphQlQuery = ' {"query": "{procedures(type: PREPARATION, pageSize: 1) { title type period currentStatus abstract voteDate submissionDate notify listType }}" }';
-  return await fetch('https://democracy.bundestag.io/graphql', {
-  method: 'post',
-    body: graphQlQuery,
-    headers: { 'Content-Type': 'application/json' }
-  }).then((response) => {
-    return response.json()
-  }).then((json) => {
-    return json.data.procedures[0];
-  });
-}
+const hcBackendUrl = 'http://localhost:3030'
+const bundestagGraphqlUrl = 'https://democracy.bundestag.io/graphql' 
 
-
-async function getAccessToken() {
-  const form = {
-  email: 'test2@test2.de',
-    password: '1234',
-    strategy: 'local',
-  }
-  return await fetch('http://localhost:3030/authentication', {
-  method: 'post',
-    body: JSON.stringify(form),
-    headers: { 'Content-Type': 'application/json' }
-  }).then((response) => {
-    return response.json()
-  }).then((json) => {
-    return json.accessToken;
-  });
-}
-
-async function contribute(accessToken, contribution) {
-  return fetch('http://localhost:3030/contributions', {
-    method: 'post',
-    body: JSON.stringify(contribution),
-    headers: {
-      'Authorization': `Bearer ${accessToken}`,
-      'Content-Type': 'application/json'
+class User {
+  login(email, password) {
+    const formData = {
+      email: email,
+      password: password,
+      strategy: 'local',
     }
-  }).then((response) => {
-    return response.json();
-  })
+    return fetch(`${hcBackendUrl}/authentication`, {
+      method: 'post',
+      body: JSON.stringify(formData),
+      headers: { 'Content-Type': 'application/json' }
+    }).then((response) => {
+      return response.json()
+    }).then((json) => {
+      this.accessToken = json.accessToken;
+      return true;
+    });
+  }
+
+  contribute(contribution){
+    return fetch(`${hcBackendUrl}/contributions`, {
+      method: 'post',
+        body: JSON.stringify(contribution),
+        headers: {
+        'Authorization': `Bearer ${this.accessToken}`,
+        'Content-Type': 'application/json'
+      }
+    }).then((response) => {
+      return response.json();
+    })
+  }
 }
+
+async function bundestagProcedures(){
+  const query = '{"query": "{procedures(type: PREPARATION, pageSize: 1) { title abstract }}"}';
+  return await fetch(bundestagGraphqlUrl, {
+  method: 'post',
+    body: query,
+    headers: { 'Content-Type': 'application/json' }
+  }).then((response) => {
+    return response.json()
+  }).then((json) => {
+    return json.data.procedures;
+  });
+}
+
+
+
 
 async function main() {
-  let firstProcedure = await getFirstProcedure();
-  let accessToken    = await getAccessToken();
+  let user = new User();
+  await user.login('test2@test2.de', '1234');
+  const procedures = await bundestagProcedures();
   const contribution = {
-    title: firstProcedure.title,
-    content: firstProcedure.abstract,
+    title: procedures[0].title,
+    content: procedures[0].abstract,
     type: "post",
     language: "de",
     categoryIds: ["5ac7768f8d655d2ee6d48fe4"] // democracy-politics
-  }
-  let response = await contribute(accessToken, contribution);
+  } 
+  const response = await user.contribute(contribution);
   console.log(response);
 }
 
